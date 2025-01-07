7cf040327fd160ab98eb69f4bb3ad9ec4e0cf69c
AARON <scottaaron@northwesternmutual.com>
2025-01-07 11:02:52 -0600

    build the collection of compnesable events more elegantly

--- src/main/java/com/blazartech/MultiDroolsDemo/LargeCalcDemoBase.java
+++ src/main/java/com/blazartech/MultiDroolsDemo/LargeCalcDemoBase.java
@@ -8,8 +8,9 @@ import com.blazartech.MultiDroolsDemo.comp.data.AmountObject;
 import com.blazartech.MultiDroolsDemo.comp.data.CompensableEvent;
 import com.blazartech.MultiDroolsDemo.comp.data.Product;
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  *
@@ -24,20 +25,20 @@ public abstract class LargeCalcDemoBase {
                 .map(AmountObject::getAmount)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
-    
-    public Collection<CompensableEvent> createCompensableEvents() {
-        Collection<CompensableEvent> compensableEvents = new ArrayList<>();
 
-        for (int i = 0; i < EVENT_COUNT; i++) {
-            CompensableEvent compensableEvent = new CompensableEvent();
-            compensableEvent.setId(1000 + i);
-            compensableEvent.setPolicyNumber("TEST" + Integer.toString(i));
-            Product product = ((i & 0x01) == 0) ? Product.VariableAnnuity : Product.WholeLife;
-            compensableEvent.setProduct(product);
-            compensableEvent.setAmount(new BigDecimal("1000"));
-            compensableEvents.add(compensableEvent);
-        }
-        
-        return compensableEvents;
+    private CompensableEvent buildCompensableEvent(int i) {
+        CompensableEvent compensableEvent = new CompensableEvent();
+        compensableEvent.setId(1000 + i);
+        compensableEvent.setPolicyNumber("TEST" + Integer.toString(i));
+        Product product = ((i & 0x01) == 0) ? Product.VariableAnnuity : Product.WholeLife;
+        compensableEvent.setProduct(product);
+        compensableEvent.setAmount(new BigDecimal("1000"));
+        return compensableEvent;
+    }
+
+    public Collection<CompensableEvent> createCompensableEvents() {
+        return IntStream.range(0, EVENT_COUNT)
+                .mapToObj(i -> buildCompensableEvent(i))
+                .collect(Collectors.toList());
     }
 }
